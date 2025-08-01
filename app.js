7050cfb049108387d8d65983137dccbd90bf1c6d
KeyBoardLessM <degelhernandezavalos@gmail.com>
2025-07-31 18:51:39 -0600

    Practica #2 - Endpoints'

--- app.js
+++ app.js
@@ -16,16 +16,58 @@ app.get("/", (req, res) => {
   const { Name, LastNames } = req.query;
   validateData(req.query, res);
   // saveInDB(req.query);
-  res.json({
-    message: `hola, ${Name} ${LastNames}!`,
-  });
 
   res.send(
-    "olita de mar" +
-      name +
-      lastnames +
+    "olita de mar " +
+      Name +
+      " " +
+      LastNames +
       ", esta es mi segunda consulta de expressjs"
   );
+});
+
+app.post("/", (req, res) => {
+  res.send("petició nde post recibida");
+});
+app.put("/", (req, res) => {
+  res.send("petición de put recibida");
+});
+app.delete("/", (req, res) => {
+  res.send("petición de delete recibida");
+});
+
+app.get("/users", (req, res) => {
+  console.log("req.query", req.query);
+  const { Name, LastNames } = req.query;
+  validateData(req.query, res);
+  // saveInDB(req.query);
+
+  res.send(
+    "olita de mar " +
+      Name +
+      " " +
+      LastNames +
+      ", esta es mi segunda consulta de expressjs de users"
+  );
+});
+
+app.post("/users", (req, res) => {
+  res.send("petició nde post recibida para users");
+});
+app.put("/users", (req, res) => {
+  res.send("petición de put recibida para users");
+});
+app.delete("/users", (req, res) => {
+  res.send("petición de delete recibida para users");
+});
+
+app.use((req, res) => {
+  validateData(req.query, res);
+  res
+    .status(404)
+    .send(
+      "  Error 404: esta ruta o pagina no tenemos planeado implementarla de manera alguna"
+    );
 });
 
 app.listen(port, () => {
